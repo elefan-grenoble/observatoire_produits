96bbf4c961bed196704e2254d15dfe0b1a43b853
Raphael Odini <raphodn@users.noreply.github.com>
2024-03-25 12:03:40 +0100

    fix(API): accélérer un peu les appels API (#9)

--- src/data/off_connector.py
+++ src/data/off_connector.py
@@ -49,20 +49,11 @@ class OFFConnector:
         Need to respect the API Constraint of a maximum of 100 requests
         per minute
         """
-        start, end, step = 0, len(barcodes), 99
-        number_of_requests = 0
-        for i in range(start, end, step):
-            x = i
-            chunk = barcodes[x : x + step]
-            for barcode in chunk:
-                if number_of_requests == 99:
-                    logger.info("Waiting 60 seconds...")
-                    time.sleep(60)
-                    number_of_requests = 0
-
-                # A valid barcode is likely to have at least 10 digits
-                if len(str(barcode)) >= 10:
-                    product_fact = self.get_product_fact(barcode)
-                    if product_fact:
-                        self.products_facts.append(product_fact)
-                    number_of_requests += 1
+        for index, barcode in enumerate(barcodes):
+            product_fact = self.get_product_fact(barcode)
+            if product_fact:
+                self.products_facts.append(product_fact)
+            # Avoid too many calls to the API
+            # 5 second sleep every 10 API calls
+            if (index % 10) == 0:
+                time.sleep(5)
