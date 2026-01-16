13072433926b863bd10c1895e063d682c99603b8
Raphael Odini <raphodn@users.noreply.github.com>
2026-01-16 01:57:46 +0100

    revert elefan changes

--- src/data/elefan_connector.py
+++ src/data/elefan_connector.py
@@ -11,17 +11,19 @@ dotenv.load_dotenv(dotenv_path)
 class ElefanConnector(EpicerieConnector):
     def __init__(self) -> None:
         super().__init__()
-        self.famille_code_food = [
-            1,  # fruits & légumes
-            2,  # épicerie salée
-            3,  # épicerie sucrée
-            4,  # boissons
-            5,  # pains & pâtisseries
-            11,  # viandes
-            12,  # produits laitiers
-            13,  # plats préparés
-            14,  # poissons
-            51,  # surgelés
+        self.codes_to_exclude = [
+            21,  # bebe (inclus nourriture)
+            22,  # hygiène, beauté
+            23,  # entretien, nettoyage
+            24,  # cuisine
+            25,  # santé
+            31,  # journaux
+            41,  # maison
+            42,  # papeterie
+            43,  # textile
+            44,  # animalerie
+            45,  # jardinerie
+            90,  # jeux
         ]
 
     def filter_products(self):
@@ -33,11 +35,7 @@ class ElefanConnector(EpicerieConnector):
         """
         self.products = [p for p in self.products if not p["code"].startswith("2")]
         self.products = [p for p in self.products if p["status"] == "ACTIF"]
-        self.products = [
-            p
-            for p in self.products
-            if p["famille"]["code"] in self.famille_code_food
-        ]
+        self.products = [p for p in self.products if p["famille"]["code"] not in self.codes_to_exclude]
 
     def extract_products_code_list(self):
         self.products_code_list = [p["code"] for p in self.products]
